499550f12171d1275581af2de92e594fdd153a91
Vihaan Dharia <vihaandharia04@gmail.com>
2024-12-30 23:21:13 +0530

    origins

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ origins = [
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['null'],   
+    allow_origins=origins,   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["GET","POST"],
